573486dd63b14db74d2281941e31c18a94f8ccca
David Mason <drdmason@gmail.com>
2015-05-01 23:28:25 +1000

    Add nested intervals within a workout.
    I could have made a separate component to hold the exercise list,
    but decided to keep it simple unless I see a good reason for a
    separate component.

--- src/workout-helper.js
+++ src/workout-helper.js
@@ -1,14 +1,29 @@
 
 /**
+ * A single interval of an exercise.
+ *
+ * exercise: (string) name of the exercise to perform during the interval
+ */
+var Interval = React.createClass({
+  render: function () {
+    return <li className="interval">
+      {this.props.exercise}
+    </li>;
+  }
+});
+
+/**
  * Widget to design and run an interval training workout.
  *
  * name: optional name for the workout
  */
 var WorkoutHelper = React.createClass({
   render: function () {
-    // Just output the workout name for now, to see props working.
     return <div className="workout">
       <h1>Workout: {this.props.name}</h1>
+      <ul>
+        {this.props.children}
+      </ul>
     </div>;
   }
 });
