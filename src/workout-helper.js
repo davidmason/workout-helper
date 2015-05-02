120e0f6568b9fbbb3ad0bb93f25ef27c9bd126c8
David Mason <drdmason@gmail.com>
2015-05-02 12:24:15 +1000

    Add clock component and use it on intervals.

--- src/workout-helper.js
+++ src/workout-helper.js
@@ -1,13 +1,42 @@
 
 /**
+ * Clock that displays a given number of seconds.
+ *
+ * Shows seconds if it is less than a minute, otherwise splits the display
+ * into minutes and seconds.
+ *
+ * seconds: (integer) total time to display
+ */
+var Clock = React.createClass({
+  render: function () {
+    var seconds = parseInt(this.props.seconds, 10);
+    var remainderSeconds = seconds % 60;
+    var secondsDisplay = <span>
+      {remainderSeconds < 10 ? '0' : null}{remainderSeconds}
+    </span>;
+
+    var minutes = Math.floor(seconds / 60);
+    var minutesDisplay = null;
+    if (minutes > 0) {
+      minutesDisplay = <span>{minutes}:</span>
+    }
+
+    return <div className="clock">
+      <i className="fa fa-clock-o"></i> {minutesDisplay}{secondsDisplay}
+    </div>
+  }
+});
+
+/**
  * A single interval of an exercise.
  *
  * exercise: (string) name of the exercise to perform during the interval
+ * time: (integer) number of seconds to perform the exercise
  */
 var Interval = React.createClass({
   render: function () {
     return <li className="interval">
-      {this.props.exercise}
+      {this.props.exercise} <Clock seconds={this.props.time} />
     </li>;
   }
 });
